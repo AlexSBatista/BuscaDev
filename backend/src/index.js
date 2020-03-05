80f6abdaa6dc39c294b782ca87368de14188a728
Alex Silva <asb2005@gmail.com>
2020-03-05 11:17:00 -0300

    Alterações feitas para integrar o back-end a parte web e mobile, conforme proposto no projeto.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://db_aplicativo:8ikjk0@cluster0-ajt2e.mongodb.net/aplicativo?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,4 +20,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
